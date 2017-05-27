5dab331640c8f26c648b7265424b941be557ed59
Bryan Hou <boyuhou@gmail.com>
2017-05-27 18:45:31 -0400

    add new logic to find owl pattern

--- sstservice/__init__.py
+++ sstservice/__init__.py
@@ -1,6 +1,17 @@
 import pandas as pd
+from sststrategy import SignalFinder as F
+from enum import Enum
 
 DATETIME_FORMAT = '%Y-%m-%d %H:%M:%S'
+LOOK_BACK_PERIOD = 10
+
+
+class Status(Enum):
+    INIT = 'Initial State'
+    AMBIGUOUS_DOWN = 'Ambiguous Downward'
+    AMBIGUOUS_UP = 'Ambiguous Up'
+    OWL_LONG = 'Owl Long'
+    OWL_SHORT = 'Owl Short'
 
 
 class SingleTestHandler(object):
@@ -41,7 +52,57 @@ class SingleDayTest(object):
         self._low = df['low'].tolist()
         self._date = df.index.tolist()
         self.ticker = ticker
+        self._state = Status.INIT
+        self.map = {
+            Status.INIT: self.initial_status,
+            Status.AMBIGUOUS_UP: self.ambiguous_up,
+            Status.AMBIGUOUS_DOWN: self.ambiguous_down,
+            # Status.OWL_LONG: self.initial_status,
+            # Status.OWL_SHORT: self.initial_status,
+        }
+
+        self._last_move_index = 0
 
     def run_test(self):
-        for curr_i in range(1, len(self._date) -1):
-            print(self._date[curr_i].strftime(DATETIME_FORMAT))
+        for curr_i in range(1, len(self._date) - 1):
+            # print(self._date[curr_i].strftime(DATETIME_FORMAT))
+            self.map.get(self._state)(curr_i + 1)
+
+    def initial_status(self, i):
+        if F.is_ambiguous_downwards_move(rl10=self.rl10[:i], bb1_lower=self.bb1_lower[:i], ambiguous_move_percent=0.85,
+                                         look_back_period=LOOK_BACK_PERIOD):
+            self._state = Status.AMBIGUOUS_DOWN
+        if F.is_ambiguous_upwards_move(rl10=self.rl10[:i], bb1_upper=self.bb1_upper[:i], ambiguous_move_percent=0.85,
+                                       look_back_period=LOOK_BACK_PERIOD):
+            self._state = Status.AMBIGUOUS_UP
+
+    def ambiguous_up(self, i):
+        if F.is_slope_negative(self.rl10[:i]) and \
+                F.is_slope_negative(self.rl30[:i]) and \
+                F.is_slope_negative(self.dragon_mean[:i]) and \
+                F.is_cross_below(self.rl10[:i], self.rl30[:i], look_back_period=LOOK_BACK_PERIOD) and \
+                F.is_cross_below(self.rl10[:i], self.dragon_lower[:i], look_back_period=LOOK_BACK_PERIOD):
+            print(self._date[:i][-1].strftime(DATETIME_FORMAT) + ' ' + 'SHORT OWL')
+            # TODO: should here enter the trade and figure out the exit strategy
+            self._state = Status.INIT
+        if not F.is_ambiguous_upwards_move(self.rl10[:i], self.bb1_upper[:i], LOOK_BACK_PERIOD, 0.85) and \
+                        (i - self._last_move_index) > LOOK_BACK_PERIOD:
+            # we here check if the price has moved a lot but took too long to reverse.
+            self._state = Status.INIT
+        self._last_move_index = i
+
+    def ambiguous_down(self, i):
+        if F.is_slope_positive(self.rl10[:i]) and \
+                F.is_slope_positive(self.rl30[:i]) and \
+                F.is_slope_positive(self.dragon_mean[:i]) and \
+                F.is_cross_above(self.rl10[:i], self.rl30[:i], look_back_period=LOOK_BACK_PERIOD) and \
+                F.is_cross_above(self.rl10[:i], self.dragon_upper[:i], look_back_period=LOOK_BACK_PERIOD):
+            print(self._date[:i][-1].strftime(DATETIME_FORMAT) + ' ' + 'LONG OWL')
+            # TODO: should here enter the trade and figure out the exit strategy
+            self._state = Status.INIT
+
+        if not F.is_ambiguous_downwards_move(self.rl10[:i], self.bb1_lower[:i], LOOK_BACK_PERIOD, 0.85) and \
+                        (i - self._last_move_index) > LOOK_BACK_PERIOD:
+            # we here check if the price has moved a lot but took too long to reverse.
+            self._state = Status.INIT
+        self._last_move_index = i
--- sststrategy/__init__.py
+++ sststrategy/__init__.py
@@ -1,3 +1,58 @@
 class SignalFinder(object):
     @staticmethod
-    def is_downwards_ambiguous_move(rl10, dragon_lower, look_back_period):
+    def is_ambiguous_downwards_move(rl10, bb1_lower, look_back_period, ambiguous_move_percent):
+        if len(rl10) < look_back_period:
+            return False
+        down_count = 0
+        for i in range(0, look_back_period):
+            reverse_index = i - look_back_period
+            if rl10[reverse_index] <= bb1_lower[reverse_index]:
+                down_count += 1
+        return float(down_count / look_back_period) >= ambiguous_move_percent
+
+    @staticmethod
+    def is_ambiguous_upwards_move(rl10, bb1_upper, look_back_period, ambiguous_move_percent):
+        if len(rl10) < look_back_period:
+            return False
+        up_count = 0
+        for i in range(0, look_back_period):
+            reverse_index = i - look_back_period
+            if rl10[reverse_index] >= bb1_upper[reverse_index]:
+                up_count += 1
+        return float(up_count / look_back_period) >= ambiguous_move_percent
+
+    @staticmethod
+    def is_slope_positive(data_series):
+        if len(data_series) < 2:
+            return False
+        return data_series[-1] / data_series[-2] >= 1
+
+    @staticmethod
+    def is_slope_negative(data_series):
+        if len(data_series) < 2:
+            return False
+        return data_series[-1] / data_series[-2] <= 1
+
+    @staticmethod
+    def is_cross_above(series_1, series_2, look_back_period):
+        if len(series_1) < look_back_period or len(series_2) < look_back_period:
+            return False
+
+        if series_1[-1] >= series_2[-1]:
+            for i in range(0, look_back_period):
+                reverse_index = i - look_back_period
+                if series_1[reverse_index] < series_2[reverse_index]:
+                    return True
+        return False
+
+    @staticmethod
+    def is_cross_below(series_1, series_2, look_back_period):
+        if len(series_1) < look_back_period or len(series_2) < look_back_period:
+            return False
+
+        if series_1[-1] <= series_2[-1]:
+            for i in range(0, look_back_period):
+                reverse_index = i - look_back_period
+                if series_1[reverse_index] > series_2[reverse_index]:
+                    return True
+        return False
